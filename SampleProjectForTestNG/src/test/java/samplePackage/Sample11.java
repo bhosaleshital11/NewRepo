fab306181ad650dc3e25cd4f8d14e924ea0a3ef1
OM <OM@DESKTOP-MJ9MOU3>
2025-02-18 16:17:39 +0530

    class Sample11

---
+++ SampleProjectForTestNG/src/test/java/samplePackage/Sample11.java
@@ -0,0 +1,13 @@
+package samplePackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sample11 {
+	@Test
+public void tasteCase1() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+	}
+}
