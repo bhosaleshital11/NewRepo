d8c190dd09d9288715e7be6945b079a577906927
91735 <91735@LAPTOP-A2KTD57B>
2025-02-18 16:26:48 +0530

    Request for practice

---
+++ SampleProjectForTestNG/src/test/java/samplePackage/Sample123.java
@@ -0,0 +1,11 @@
+package samplePackage;
+
+import org.testng.annotations.Test;
+
+public class Sample123 {
+
+	@Test
+	public void SampleTest() {
+		System.out.println("sending pull request just foe practice");
+	}
+}
