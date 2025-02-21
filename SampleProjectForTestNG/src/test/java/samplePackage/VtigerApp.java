710ed573736509d62d37738c94f6d4a7a37f99d8
SANMESH <SANMESH@MSI>
2025-02-21 18:00:30 +0530

    Write Script In VtigerApp

--- SampleProjectForTestNG/src/test/java/samplePackage/VtigerApp.java
+++ SampleProjectForTestNG/src/test/java/samplePackage/VtigerApp.java
@@ -1,10 +1,23 @@
 package samplePackage;
 
+import org.testng.annotations.Test;
+
 public class VtigerApp {
 
-	public static void main(String[] args) {
+
+	@Test (dependsOnMethods ="SampeTest2")
+	public void sampleTest()
+	{
+		System.out.println("method 1");
+	}
+	
+	@Test
+	public void SampeTest2()
+	{
+		System.out.println("method 2");
+	}
 		// TODO Auto-generated method stub
 
 	}
 
-}
+
