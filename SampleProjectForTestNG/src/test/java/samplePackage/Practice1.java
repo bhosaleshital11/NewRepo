35071c275008259191ca7adf660f01c31583644e
Akshay <Akshay@LAPTOP-UDLNG9US>
2025-02-21 17:54:50 +0530

    Updated class

---
+++ SampleProjectForTestNG/src/test/java/samplePackage/Practice1.java
@@ -0,0 +1,10 @@
+package samplePackage;
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrgTest");
+	}
+
+}
